500af040205b16eed737e0e3a667ed9860ec8146
aeyeon96 <dldodus6487@gmail.com>
2022-09-05 11:49:23 +0900

    [update]

--- src/MiniProjectConnectionMysql_me.java
+++ src/MiniProjectConnectionMysql_me.java
@@ -4,39 +4,52 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MiniProjectConnectionMysql_me {
-    
+public class Java_Mysql_countty_me {
     public static void main(String[] args) {
-        // Mysql 열기 -> mysql-connector J
-        // Connect to database 클릭
-        // Connection Method, hostname, port, username을 맞춘후
-        // 들어갈 Default Schema 를 작성후 Connection
-        // Hostname : localhost, port : 3306 
-        // Username : root, default schema : world, passworld : tbrs00002b
-        final String USER = "root";
-        final String PASS = "tbrs00002b";
-        final String DB_URL = "jdbc:mysql://localhost/world";
-        String QUERY = "select Code, Name, Continent, Region " +
-                        "from country " +
-                        "where Name like ('an%')";
-        try {
-            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = conn.createStatement(); // sql파일 만드는 구문
-        // 새로운 query생성, select 명령문 입력 후 test 해보기
-        ResultSet rs = stmt.executeQuery(QUERY); //결과 값이 담기는 집합
-        // result grid, action output 확인 
-        // 갯수를 모를땐 while 안다면 for문을 사용
-            while (rs.next()) {
-                // Retrieve by column name
-                System.out.print("Code: " + rs.getString("Code"));
-                System.out.print(", Name: " + rs.getString("Name"));
-                System.out.print(", Continent: " + rs.getString("Continent"));
-                System.out.print(", Region: " + rs.getString("Region"));
-            }
+        // VS code에서 폴더 만들기, Class 파일 만들기
+        // Connect to database
+            // Access to SQL server --> Mysql-connector J (Already done)
+            // Check host name
+            // Check user
+            // Check Schema
+            // Check password
+            // Connection Method -jdbc (java DB connection)
+            final String DB_URL = "jdbc:mysql://localhost/country";
+            final String USER = "localhost";
+            final String PASS = "tbrs00002b";
+            try {
+                // Connect to the 'world' database
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                
+                // Open a statement (Edit window)
+                Statement stmt = conn.createStatement();
+                
+                // Input Query on statement
+                String QUERY = "select Code, Name, Continent, Capital "+
+                "from country "+
+                "Where 1 = 1 "+
+                "and Name like \"%an&\";";
+
+                // Insert the output from the execution into the 'rs' variable
+                ResultSet rs = stmt.executeQuery(QUERY);
             
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return ;
+                // Chekc the grid (line by line) and the result & action output
+                while (rs.next()){
+                    // Retrieve by column name
+                    System.out.print("Code: " + rs.getInt("Code"));
+                    System.out.print(", NAME: " + rs.getString("NAME"));
+                    System.out.print(", Continent: " + rs.getString("Continent"));
+                    System.out.print(", Capital: " + rs.getString("Capital"));
+                    
+                }
+            // Get result from Query by row (Top-Down way)
+            } catch (SQLexception e){
+                e.printStackTrace();
+            }
+            return;
+        // Make Statments
+        
+        
+
     }
 }
